430e248c28ee1af8bee3942352e27de1990acbf7
sam5213 <gulmirazhb@gmail.com>
2025-01-17 16:37:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 document.getElementById('wakeButton').addEventListener('click', function() {
     const catImage = document.getElementById('catImage');
-    catImage.src = 'wakeupCat.jpeg'; // Измените на URL проснувшегося кота
-    catImage.alt = 'Проснувшийся кот';
+    const message = document.getElementById('message');
+
+    // Показать сообщение о том, что кот услышал
+    message.textContent = 'Кот услышал и скоро проснется...';
+    sendGetRequest();
+
+    // Через 30 секунд поменять изображение и сообщение
+    setTimeout(function() {
+        catImage.src = 'wakeupCat.jpeg'; // URL для изображения проснувшегося кота
+        catImage.alt = 'Проснувшийся кот';
+        message.textContent = 'Кот проснулся!';
+    }, 30000); // 30000 миллисекунд = 30 секунд
 });
+
+function sendGetRequest() {
+    var xhr = new XMLHttpRequest(); // Создаем новый объект XMLHttpRequest
+    var url = "https://wakeupquizbot.glitch.me"; // URL, на который будет отправлен запрос
+
+    xhr.open("GET", url, true); // Инициализируем GET-запрос
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) { // Проверяем, завершен ли запрос
+            if (xhr.status === 200) { // Проверяем, успешен ли ответ
+                console.log("Response:", xhr.responseText); // Выводим ответ в консоль
+            } else {
+                console.error("Error:", xhr.status); // Выводим ошибку, если запрос не успешен
+            }
+        }
+    };
+    
+    xhr.send(); // Отправляем запрос
+};
