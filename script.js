88d8da66cfd8d6d0709d385391ec6599444b611b
sam5213 <gulmirazhb@gmail.com>
2025-02-16 14:40:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.getElementById('wakeButton').addEventListener('click', function() {
         if (progress >= 100) {
             clearInterval(interval);
         }
-    }, 800); // Обновляем каждые 800 миллисекунд
+    }, 1400); // Обновляем каждые 1400 миллисекунд
 
     setTimeout(function() {
         catImage.src = 'wakeupCat.jpeg'; // URL для изображения проснувшегося кота
